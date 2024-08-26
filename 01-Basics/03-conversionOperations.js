04afa4f3c5f3010bf2e18cde4d20f9d702be7c53
Saksh <ss.sakshamsrivastava02@gmail.com>
2024-08-26 09:05:03 +0000

    Conversion of datatypes

---
+++ 01-Basics/03-conversionOperations.js
@@ -0,0 +1,28 @@
+let score = "69"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abddhg" => NaN but still a number type
+// true => 1; false => 0
+
+let isLoggedIn = true
+
+let checkIn = String(isLoggedIn);
+// console.log(checkIn);
+// console.log(typeof checkIn);
+
+let identifyTheNumber = true
+
+let numberIsFine = Number(identifyTheNumber)
+// console.log(numberIsFine);
+// console.log(typeof numberIsFine);
+
+let bool = ""
+let convertIt = Boolean(bool)
+console.log(convertIt);
+console.log(typeof convertIt);
