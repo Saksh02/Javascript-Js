c782873c7b77c0e0c7a8ccca9864d3d3f52a4257
Saksh <ss.sakshamsrivastava02@gmail.com>
2024-08-27 18:05:20 +0000

    Comparison-Operators done

--- 01-Basics/04-Comparisons.js
+++ 01-Basics/04-Comparisons.js
@@ -5,3 +5,20 @@
 // console.log(23 != 1);
 
 
+// console.log("343" > 11);
+// console.log("0343" > 11);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null != 0);
+// console.log(null === 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined === 0);
+
+// === strictly checking, if datatypes are same
+
+console.log("28" === 28);
+
+
